718bf7c81750535fdedb73f23ee3d2deea983c00
TheprogrammerRohith <rohithkumar0425@gmail.com>
2024-03-23 13:25:08 +0530

    additional files

--- app/src/main/java/com/example/expensetracker/DashboardFragment.java
+++ app/src/main/java/com/example/expensetracker/DashboardFragment.java
@@ -1,64 +1,150 @@
 package com.example.expensetracker;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DashboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.expensetracker.Model.Data;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+
 public class DashboardFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FloatingActionButton add,income,expense;
+    private TextView income_text,expense_text;
+    private boolean flag=true;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DashboardFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
-        DashboardFragment fragment = new DashboardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FirebaseAuth mAuth;
+    private DatabaseReference incomedata;
+    private DatabaseReference expensedata;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        mAuth= FirebaseAuth.getInstance();
+        FirebaseUser user=mAuth.getCurrentUser();
+        String uid=user.getUid();
+        incomedata= FirebaseDatabase.getInstance().getReference().child("Income").child(uid);
+
+        add=view.findViewById(R.id.add_ft_btn);
+        income=view.findViewById(R.id.income_ft_btn);
+        expense=view.findViewById(R.id.expense_ft_btn);
+        income_text=view.findViewById(R.id.income_ft_text);
+        expense_text=view.findViewById(R.id.expense_ft_text);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(flag){
+                    income_text.setVisibility(View.VISIBLE);
+                    income.setVisibility(View.VISIBLE);
+                    expense_text.setVisibility(View.VISIBLE);
+                    expense.setVisibility(View.VISIBLE);
+                    addData();
+                    flag=false;
+                }
+                else{
+                    income_text.setVisibility(View.INVISIBLE);
+                    income.setVisibility(View.INVISIBLE);
+                    expense_text.setVisibility(View.INVISIBLE);
+                    expense.setVisibility(View.INVISIBLE);
+                    flag=true;
+                }
+
+            }
+        });
+
+        return view;
+    }
+
+    private void addData() {
+        income.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                income_text.setVisibility(View.INVISIBLE);
+                income.setVisibility(View.INVISIBLE);
+                expense_text.setVisibility(View.INVISIBLE);
+                expense.setVisibility(View.INVISIBLE);
+                flag=true;
+                incomeDataInsert();
+            }
+
+        });
+
+        expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void incomeDataInsert(){
+        AlertDialog.Builder mydialog=new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater=LayoutInflater.from(getActivity());
+        View view=inflater.inflate(R.layout.dialog_layout,null);
+        mydialog.setView(view);
+        AlertDialog dialog=mydialog.create();
+
+        EditText edtAmount=view.findViewById(R.id.amount_edt);
+        EditText edtType=view.findViewById(R.id.type_edt);
+        EditText edtNote=view.findViewById(R.id.note_edt);
+        Button cancel=view.findViewById(R.id.btncancel);
+        Button save=view.findViewById(R.id.btnsave);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String amount=edtAmount.getText().toString();
+                String type=edtType.getText().toString();
+                String note=edtNote.getText().toString();
+                if(amount.equals("")){
+                    edtAmount.setError("Required Field ..");
+                    return;
+                }
+                int Amount=Integer.parseInt(amount);
+                if(type.equals("")){
+                    edtType.setError("Required Field ..");
+                    return;
+                }
+                if(note.equals("")){
+                    edtNote.setError("Required Field ..");
+                    return;
+                }
+                String id=incomedata.push().getKey();
+                String date= DateFormat.getDateInstance().format(new Date());
+                Data data=new Data(Amount,type,note,id,date);
+                incomedata.child(id).setValue(data);
+                Toast.makeText(getActivity(),"Details are stored",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
